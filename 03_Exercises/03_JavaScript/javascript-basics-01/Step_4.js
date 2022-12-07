426e397982344ecad7f7d11f779aa772dfe7e9d1
Assala <ismailassala1@gmail.com>
2022-12-07 13:21:56 +0200

    Step4 done

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_4.js
@@ -0,0 +1,5 @@
+let name1 = prompt("What's your last name ?")
+let surname= prompt("What's your first name ?")
+let city= prompt("What is your city ?")
+
+alert("Hello, " + surname + " " + name1 + "\n Oh your city is " + city + "!" );
