a96f3230596619ed49788a8d9fd2968da2d362ed
Assala <ismailassala1@gmail.com>
2022-12-08 08:49:13 +0200

    done

--- 03_Exercises/03_JavaScript/javascript-basics-01/Step_4.js
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_4.js
@@ -1,12 +1,8 @@
-
-function a()
-{
-  name1 = document.getElementById(
-    "name").value;
-  name2 = document.getElementById(
-    "surname").value;
+function a(){
+  name1 = document.getElementById( "name").value;
+  name2 = document.getElementById("surname").value;
     city= document.getElementById("city").value;
   alert("Hello, " + (document.getElementById(
-    "result").innerHTML = name1 + name2) + "\n Oh your city is " + 
+    "result").innerHTML = name1 + name2) + "\n your city is " + 
     (document.getElementById("result").innerHTML = city));
 }
---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_7.js
@@ -0,0 +1,11 @@
+function a()  { 
+   var shoe = document.getElementById( "shoe_size").value;
+     var birth = document.getElementById("year").value;
+     var m2= shoe*2;
+     var add=m2+5;
+     var m50= add*50;
+     var sub= m50-birth;
+     var result= sub+1766;
+     alert(result)
+     
+    }
