7f017208f5ee892a75b7f0f3d00602c2ed3938ee
Assala <ismailassala1@gmail.com>
2022-12-08 11:33:25 +0200

    done4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 4/step_4.js
@@ -0,0 +1,16 @@
+    
+      function resetDatabase() {
+        document.getElementById("confirm").hidden=false
+      }
+      
+      //redirect to the delete profile script
+    function confirmYes() {
+        
+            document.getElementById("form").reset();
+          }
+      
+      
+      //hide the confirmation div
+      function confirmNo() {
+        document.getElementById("confirm").hidden=true
+      }
