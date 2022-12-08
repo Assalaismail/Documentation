9c808f1117a0eb0c1838b7b7eccb418996035e25
Assala <ismailassala1@gmail.com>
2022-12-08 13:12:50 +0200

    done3.4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 4/step 4.js
@@ -0,0 +1,12 @@
+
+ function test(){
+    var pass=document.querySelector("#password").value;
+    var x=document.getElementById("password");
+    var conf=document.querySelector("#confirmation").value;
+    var y=document.getElementById("confirmation");
+    if(pass!=conf){
+        x.style.border="3px solid red";
+        y.style.border="3px solid red";
+    }
+   
+    }
