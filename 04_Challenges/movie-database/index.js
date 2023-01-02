7f217c625eed011a184083ec303d79172737ca26
Assala <ismailassala1@gmail.com>
2023-01-02 22:39:18 +0200

    step3

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -6,3 +6,23 @@ const port = 3000
 app.listen(port, () => {
   console.log(`ok!`)
 })
+
+app.get('/', (request, response) => {
+    response.send('Hello');
+  });
+
+app.get('/test', (request, response) => {
+    response.send({
+        status:200, 
+        message:"ok"
+    });
+})
+app.get('/time', (request, response) => {
+    const time = new Date();
+    const specifictime = `${time.getHours()}:${time.getSeconds()}`;
+    const time1 = {
+        status : 200,
+        message : specifictime
+    }
+    response.send(time1);
+})
